fddb287721eb4803e968618a2b1627011e9f96f1
kiranwithcode <kirankamble4595@gmail.com>
2022-06-28 22:52:49 +0530

    server added

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: 'https://m.media-amazon.com/images/I/61RqedeafJL._AC_UL480_FMwebp_QL65_.jpg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: 'https://m.media-amazon.com/images/I/41Aemtzsa3L._AC_UL480_FMwebp_QL65_.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: 'https://m.media-amazon.com/images/I/71vz6n+YCLL._AC_UL480_FMwebp_QL65_.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: 'https://m.media-amazon.com/images/I/71t2MOBgtcL._AC_UL480_FMwebp_QL65_.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.7,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
