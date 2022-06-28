04d73a11b94ad0acdaba7e196f65268e34f8130e
kiranwithcode <kirankamble4595@gmail.com>
2022-06-28 17:24:11 +0530

    frontend changes

---
+++ myamazon/src/screens/HomeScreen.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import data from '../data';
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <h1>Feature Products</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`product/${product.slug}`}>
+              <img src={product.image} alt="" />
+            </Link>
+            <div className="product-info">
+              <Link to={`product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>{product.price}</p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreen;
