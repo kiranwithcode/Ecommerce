8e5c0dd0a8b1f76d657756bca7ba08611fad68d9
kiranwithcode <kirankamble4595@gmail.com>
2022-06-28 12:15:39 +0530

    ecommerce app

---
+++ myamazon/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div >
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
