2fdd7ddb093c9e05159a1dcce695ebd78c2f090a
kiranwithcode <kirankamble4595@gmail.com>
2022-07-02 22:28:39 +0530

    product inc and dec added

--- myamazon/src/components/Product.jsx
+++ myamazon/src/components/Product.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useContext } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -5,11 +6,25 @@ import { Store } from '../Store';
 import Rating from './Rating';
 
 const Product = ({ product }) => {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
 
-  const {state, dispatch:ctxDispatch} = useContext(Store)
-  const addToCartHandler = () =>{
-      ctxDispatch({type:'CART_ADD_ITEM', payload: {...product, quantity:1}})
+  const addToCartHandler = async (item) =>{
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
   }
+
   return (
     <Card className='set-res'>
       <Link to={`product/${product.slug}`}>
@@ -20,8 +35,10 @@ const Product = ({ product }) => {
           <Card.Title>{product.name}</Card.Title>
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
-        <Card.Text>${product.price}</Card.Text>
-        <Button onClick={addToCartHandler}>Add to cart</Button>
+        <Card.Text>₹{product.price}</Card.Text>
+        {product.countInStock === 0? <Button variant='light' disabled>Out of Stock</Button>:
+        <Button onClick={() => addToCartHandler(product)}>Add to cart</Button>
+      }
       </Card.Body>
       
     </Card>
