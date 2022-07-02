01be7f94e8f07fcfdd22e96e976fc603d213e71c
kiranwithcode <kirankamble4595@gmail.com>
2022-07-02 14:01:27 +0530

    added cartScreen

--- myamazon/src/screens/ProductScreen.jsx
+++ myamazon/src/screens/ProductScreen.jsx
@@ -9,7 +9,7 @@ import {
   ListGroupItem,
   Row,
 } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
@@ -30,6 +30,7 @@ const reducer = (state, action) => {
   }
 };
 const ProductScreen = () => {
+ const navigate  = useNavigate()
   const param = useParams();
   const { slug } = param;
 
@@ -65,6 +66,7 @@ const ProductScreen = () => {
       type: 'CART_ADD_ITEM',
       payload: { ...product, quantity },
     });
+    navigate('/cart')
   };
 
   return loading ? (
